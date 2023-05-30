2349de8a335912b204a77c5ffdbaf6c3670f89a7
buddhimac111 <91274532+buddhimac111@users.noreply.github.com>
2023-05-30 11:38:56 +0530

    ngrok

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -28,7 +28,7 @@ const Home = () => {
         <div className="text-center my-3">
           <p className="headerTopic">Data Sheets Comparison</p>
         </div>
-        <Form encType="multipart/form-data" method="POST" action="https://datasheets.onrender.com/upload">
+        <Form encType="multipart/form-data" method="POST" action="https://ec18-123-231-125-62.ngrok-free.app/upload">
           <div className="w-100 container p-3">
             <FormGroup>
               <Label for="file">Select Your Files</Label>
